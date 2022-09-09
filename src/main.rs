b16482934bea6887898def88ec4b6b1a7062df8b
henri <henri1i.dev@gmail.com>
2022-09-08 23:01:02 -0300

    feat: make small progress on forwarding the requests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-use std::net::SocketAddrV4;
-
+use std::borrow::BorrowMut;
+use std::convert::Infallible;
+use std::f32::consts::E;
+use std::net::SocketAddr;
+use std::collections::LinkedList;
+use std::sync::Arc;
 use clap::{Command, Arg};
+use hyper::client::{conn, connect};
+use hyper::header::HOST;
+use hyper::http::{HeaderValue, request};
+use hyper::server::conn::AddrStream;
+use hyper::service::{make_service_fn, service_fn, Service};
+use hyper::{Body, Request, Response, Client, Method};
+use hyper::server::Server;
+use tokio::sync::Mutex;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = Command::new("Load Balancer")
         .arg(Arg::new("server")
             .short('s')
@@ -17,14 +30,56 @@ fn main() {
             .help("Value that the load balancer will be binded to"))
         .get_matches();
 
-    let bind: SocketAddrV4 = matches.value_of("bind").expect("Load balancer address is needed.").parse().unwrap();
+    let bind_address = matches.value_of("bind")
+        .expect("Load balancer address is needed.");
 
-    let servers: Vec<SocketAddrV4> = matches.values_of("server")
+    let available_servers: LinkedList<SocketAddr> = matches.values_of("server")
         .expect("At least one server is needed")
         .into_iter().map(|address| {
-            address.parse().expect("Invalid IPv4 format")
+            address.parse().expect("Invalid address format")
         })
         .collect();
 
-    println!("{:#?} {}", servers, bind);
+    let available_servers = Arc::new(Mutex::new(available_servers));
+
+    let make_service = make_service_fn(move |conn: &AddrStream| {
+
+        let available_servers = available_servers.clone();
+
+        let service = service_fn(move |mut request| {
+            forward_request(request, available_servers.clone())
+        });
+
+        async move { Ok::<_, Infallible>(service) }
+    });
+
+    let server = Server::bind(&bind_address.parse::<SocketAddr>().unwrap())
+        .serve(make_service);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e)
+    }
+}
+
+// TODO: Treat all those fucked up unwraps
+async fn forward_request(request: Request<Body>, available_servers: Arc<Mutex<LinkedList<SocketAddr>>>) -> Result<Response<Body>, Infallible> {
+    let client = Client::new();
+
+    let mut available_servers = available_servers.lock().await;
+
+    let server = available_servers.pop_front().unwrap();
+
+    let request = Request::builder()
+        .uri(dbg!(server.to_string()))
+        .method(request.method())
+        .body(Body::from("pog")).unwrap();
+
+    let response = client.request(request).await;
+
+    available_servers.push_back(server);
+
+    match response {
+        Ok(response) => Ok(response),
+        Err(response) => panic!("WE FUCKED!!! take a look: {:#?}", response)
+    }
 }
